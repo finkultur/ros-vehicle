8048d3f00e85db0656c232137f508a49c57304c7
Viktor Nilsson <viktor.w.nilsson@gmail.com>
2015-01-14 18:06:13 +0100

    Revert last commit, was just a typo

--- catkin_ws/src/autopilot/scripts/ap_utils.py
+++ catkin_ws/src/autopilot/scripts/ap_utils.py
@@ -111,8 +111,8 @@ def is_point_behind_line(x,y,px0,py0,px1,py1):
 # This is needed since all image viewers has (0,0) in top left corner
 # [(1,2),(3,4)] => [(1,-2),(3,-4)]
 def negate_y(array):
-  for i in range(len(array)):
-    (x,y) = array[i]
-    array[i] = (x,-y)
+  for i, (x,y) in enumerate(array):
+     array[i] = (x,-y)
   return array
+
 
