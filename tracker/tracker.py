aacb7b3e891aa39028cdb7d06d5d7faf83f610c8
Viktor Nilsson <viktor.w.nilsson@gmail.com>
2015-01-17 15:21:26 +0100

    tracker: Fixed rounding error and other stuff

--- tracker/tracker.py
+++ tracker/tracker.py
@@ -26,8 +26,8 @@ def main():
   mapbg = pygame.image.load('map_700.jpg')
   font = pygame.font.SysFont("monospace", 15)
   label = font.render("x=0, y=0", 1, (0,0,0))
-  id_text = font.render("no waypoint here", 1, (0,0,0))
-  track = read_track()
+  id_text = font.render("-", 1, (0,0,0))
+  track,g_track = read_track()
   state = "normal"
   fst_point = None 
   snd_point = None
@@ -37,8 +37,8 @@ def main():
 
     # Draw waypoints with alternating colors
     color = pygame.Color(0,255,0)
-    for point in track:
-      color = change_color2(color)
+    for point in g_track:
+      color = change_color(color)
       pygame.draw.circle(screen, color, (point), 6)
 
     for event in pygame.event.get():
@@ -46,49 +46,50 @@ def main():
         pygame.quit()
         sys.exit()
       if event.type == MOUSEMOTION:
-        x,y = event.pos
+        m_x,m_y = event.pos
+        x,y = scale_point(m_x,m_y)
+        label = font.render("x=%.2f, y=%.2f" % (x,y), 1, (0,0,0))
         # If it is a waypoint here, print the number
-        if in_track(track,x,y):
-          wp_id = track.index(where_in_track(track,x,y))
+        if in_track(g_track,m_x,m_y):
+          wp_id = where_in_track(g_track,m_x,m_y)[0]
           where = "wp #%i" % wp_id
           id_text = font.render(where, 1, (0,0,0))
         else:
-          id_text = font.render("", 1, (0,0,0))
-        (x,y) = scale_point(x,y)
-        pos = "x=%.2f, y=%.2f" % (x,y)
-        label = font.render(pos, 1, (0,0,0))
+          id_text = font.render("-", 1, (0,0,0))
       elif event.type == MOUSEBUTTONDOWN:
-        x,y = event.pos
+        m_x,m_y = event.pos
         # Add a point in between two other
         if state == "add_between":
-          if not fst_point and in_track(track,x,y):
-            fst_point = where_in_track(track,x,y)
-          elif not snd_point and in_track(track,x,y):
-            snd_point = where_in_track(track,x,y)
-          elif fst_point and snd_point and fst_point != snd_point:
-            track.insert(track.index(snd_point), event.pos)
-            (x,y) = scale_point(x,y)
-            pos = "(%.2f,-%.2f)" % (x, y)
-            print(pos)
+          if not fst_point and in_track(g_track,m_x,m_y):
+            fst_point = where_in_track(g_track,m_x,m_y)
+          elif not snd_point and in_track(g_track,m_x,m_y):
+            snd_point = where_in_track(g_track,m_x,m_y)
+          elif fst_point and snd_point and fst_point != snd_point and \
+               (abs(fst_point[0] - snd_point[0])) == 1 :
+            x,y = scale_point(m_x,m_y)
+            track.insert(snd_point[0], (x,y))
+            g_track.insert(snd_point[0], (m_x,m_y))
+            print("(%.2f,%.2f)" % (x,y))
             fst_point = None
             snd_point = None
             state = "normal"
           else:
-            print("There is no existing point there")
+            print("You're doing it wrong, press 'R' for normal mode")
         # If not in track, create new waypoint
-        elif not in_track(track,x,y):
-          track.append(event.pos)
-          (x,y) = scale_point(x,y)
-          pos = "(%.2f,-%.2f)" % (x, y)
-          print(pos)
+        elif not in_track(g_track,m_x,m_y):
+          track.append((x,y))
+          g_track.append((m_x,m_y))
+          print("(%.2f,%.2f)" % (x,y))
         # Else remove the existing waypoint
         else:
-          x1,y1 = where_in_track(track,x,y)
-          track.remove((x1,y1))
-          print("Removed (%.2f,-%.2f)" % scale_point(x1,y1))
+          (i,x1,y1) = where_in_track(g_track,m_x,m_y)
+          print("Removed (%.2f,%.2f)" % (track[i][0],track[i][1]))
+          track.pop(i)
+          g_track.pop(i)
       # Clear track
       elif event.type == KEYDOWN and event.key == K_c:
         del track[:]
+        del g_track[:]
         track_str = ""
         print("Cleared track")
       # Print track
@@ -111,22 +112,24 @@ def main():
           state = "normal"
           print("Normal mode")
 
-    screen.blit(label, (150,110))
-    screen.blit(id_text, (190,170))
+    screen.blit(label, (150,540))
+    screen.blit(id_text, (190,560))
     pygame.display.update()
     fpsClock.tick(30)
 
 def read_track():
   if len(sys.argv) == 2:
+    g_track = []
     with open (sys.argv[1], "r") as track_file:
       track = literal_eval(track_file.read())
-    print(track[0][0])
     if not track or type(track[0]) is not tuple or type(track[0][0]) is not float:
       track = []
     else:
-      for i, (x,y) in enumerate(track):
-        track[i] = rescale_point(x,y)
-  return track
+      for (x,y) in track:
+        g_track.append(rescale_point(x,y))
+    return (track, g_track)
+  else:
+    return ([],[])
 
 def in_track(track,x,y):
   for x1,y1 in track:
@@ -135,9 +138,9 @@ def in_track(track,x,y):
   return False
 
 def where_in_track(track,x,y):
-  for x1,y1 in track:
+  for i, (x1,y1) in enumerate(track):
     if abs(x-x1) <= 6 and abs(y-y1) <= 6:
-      return (x1,y1)
+      return (i,x1,y1)
 
 def track_to_string(track):
   if not track: 
@@ -145,13 +148,12 @@ def track_to_string(track):
   else:
     track_str = "["
     for (x,y) in track:
-      (x,y) = scale_point(x,y)
-      track_str += "(%.2f,-%.2f)," % (x,y)
+      track_str += "(%.2f,%.2f)," % (x,y)
     return track_str[:-1] + "]"
 
 def scale_point(x,y):
-  x = float(x) / 0.7 / 100
-  y = float(y) / 0.7 / 100
+  x = float(x) / 70
+  y = -float(y) / 70
   return (x,y)
 
 def rescale_point(x,y):
@@ -160,12 +162,6 @@ def rescale_point(x,y):
   return (x,y)
 
 def change_color(color):
-  r,g,b = color
-  r = (r+15) % 255
-  g = (g-15) % 255
-  return (r,g,b)
-
-def change_color2(color):
   color.r = (color.r+15) % 255
   color.g = (color.g-15) % 255
   return color
