ddec66d7bba18c883f9b6fa39c5e000f8c7785d1
Viktor Nilsson <viktor.w.nilsson@gmail.com>
2015-01-19 18:43:03 +0100

    Fixed indentation

--- catkin_ws/src/bldc_mc/src/Serial.hpp
+++ catkin_ws/src/bldc_mc/src/Serial.hpp
@@ -25,84 +25,79 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-
 #ifndef SERIAL_H
 #define SERIAL_H
 
 #include <boost/asio.hpp>
 #include <iostream>
 
-class Serial
-{
-	private:
-		boost::asio::io_service* io;
-		boost::asio::serial_port* serial_port;
-		boost::asio::deadline_timer* timer;
-		bool loop;
-		char c;
-		
+class Serial {
+  private:
+    boost::asio::io_service* io;
+    boost::asio::serial_port* serial_port;
+    boost::asio::deadline_timer* timer;
+    bool loop;
+    char c;
 
-	public:
-		/**
-		 * Constructor.
-		 * \param port device name, example "/dev/ttyUSB0" or "COM4"
-		 * \param baud_rate communication speed, example 9600 or 115200
-		 * \throws boost::system::system_error if cannot open the
-		 * serial device
-		 */
-		Serial(std::string port, unsigned int baud_rate)
-		{
-			io = new boost::asio::io_service();
-			serial_port = new boost::asio::serial_port(*io);
-			serial_port->open(port);
-		    serial_port->set_option(boost::asio::serial_port_base::baud_rate(baud_rate));
-		}
+  public:
+    /**
+     * Constructor.
+     * \param port device name, example "/dev/ttyUSB0" or "COM4"
+     * \param baud_rate communication speed, example 9600 or 115200
+     * \throws boost::system::system_error if cannot open the
+     * serial device
+     */
+    Serial(std::string port, unsigned int baud_rate) {
+      io = new boost::asio::io_service();
+      serial_port = new boost::asio::serial_port(*io);
+      serial_port->open(port);
+      serial_port->set_option(boost::asio::serial_port_base::baud_rate(baud_rate));
+    }
 
-		~Serial()
-		{
-			serial_port->close();
-			delete io;
-			delete serial_port;
-			delete timer;
-		}
+    ~Serial() {
+      serial_port->close();
+      delete io;
+      delete serial_port;
+      delete timer;
+    }
 
-		void writeString(uint8_t* s, size_t lenght)
-		{
-		    boost::asio::write(*serial_port,boost::asio::buffer(s,lenght));
-		}
+    void writeString(uint8_t* s, size_t lenght) {
+        boost::asio::write(*serial_port,boost::asio::buffer(s,lenght));
+    }
 
-		// Called when an async read completes or has been cancelled
-   		void read_complete(const boost::system::error_code& error, size_t bytes_transferred) 
-		{        
-		    // Read has finished, so cancel the timer
-			timer->cancel();
-		}
-		
-		void time_out(const boost::system::error_code& error)
-		{
-			if(c == 0)
-				serial_port->cancel();				
-		}
+    // Called when an async read completes or has been cancelled
+    void read_complete(const boost::system::error_code& error, 
+                       size_t bytes_transferred) {        
+      // Read has finished, so cancel the timer
+      timer->cancel();
+    }
+    
+    void time_out(const boost::system::error_code& error) {
+      if(c == 0) {
+        serial_port->cancel();
+      }
+    }
 
-		char readChar()
-		{
-			timer = new boost::asio::deadline_timer(serial_port->get_io_service());
+    char readChar() {
+      timer = new boost::asio::deadline_timer(serial_port->get_io_service());
 
-			c = 0;
-			serial_port->get_io_service().reset();
-			boost::asio::async_read(*serial_port, boost::asio::buffer(&c, 1), boost::bind(&Serial::read_complete, 
+      c = 0;
+      serial_port->get_io_service().reset();
+      boost::asio::async_read(*serial_port, boost::asio::buffer(&c, 1), 
+                              boost::bind(&Serial::read_complete, 
+                              this, 
+                              boost::asio::placeholders::error, 
+                              boost::asio::placeholders::bytes_transferred)); 
+
+      timer->expires_from_now(boost::posix_time::milliseconds(500));
+      timer->async_wait(boost::bind(&Serial::time_out,
                         this, 
-                        boost::asio::placeholders::error, 
-                        boost::asio::placeholders::bytes_transferred)); 
+                        boost::asio::placeholders::error));
 
-			timer->expires_from_now(boost::posix_time::milliseconds(500));
-        	timer->async_wait(boost::bind(&Serial::time_out,
-                                this, boost::asio::placeholders::error));
+      serial_port->get_io_service().run();
 
-			serial_port->get_io_service().run();
-
-			return c;
-		}
+      return c;
+    }
 };
 
 #endif
